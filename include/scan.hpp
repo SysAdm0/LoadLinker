b488ed03fe994ab2a8d7a55d48915a4225231b5e
Lucien <lucien.carneiro@protonmail.com>
2023-11-03 21:55:01 -0400

    add: tcp socket connection

--- include/scan.hpp
+++ include/scan.hpp
@@ -1,10 +1,20 @@
 #pragma once
 #include "nginx.hpp"
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 class scan {
     public:
         scan() = default;
         ~scan() = default;
 
-        static void scan_network(std::string subnet, std::string netmask);
+        void scan_network(std::string subnet, std::string netmask);
+
+    protected:
+        int send_tcp_socket(std::string ip_address);
+
+    private:
+        int port = 80;
 };
--- src/scan.cpp
+++ src/scan.cpp
@@ -1,5 +1,29 @@
 #include "scan.hpp"
 
+int scan::send_tcp_socket(std::string ip_address) {
+    struct sockaddr_in host_addr;
+    int fd;
+
+    std::cout << "Sending TCP socket to " << ip_address << std::endl;
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        return 1;
+
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(this->port);
+
+    if (inet_pton(AF_INET, ip_address.data(), &host_addr.sin_addr) <= 0)
+        return 1;
+    if (connect(fd, (struct sockaddr*)&host_addr, sizeof(host_addr)) < 0)
+        return 1;
+
+    close(fd);
+    return 0;
+}
+
 void scan::scan_network(std::string subnet, std::string netmask) {
-    std::cout << "Scanning network " << subnet << "/" << netmask << std::endl;
+    std::cout << "Scanning network... " << netmask << std::endl;
+    if (send_tcp_socket(subnet) == 0)
+        std::cout << "Found a host!" << std::endl;
+    else
+        std::cout << "No hosts found!" << std::endl;
 }
