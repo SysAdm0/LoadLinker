b4ee88d3ea1e3093e88a31c3134ed4d8bb4223c2
Lucien <lucien.carneiro@protonmail.com>
2023-11-20 12:38:41 -0500

    add: new install release

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 ## LoadLinker installer script for Linux ##
 
 CONFIG=$1
-VERSION="1.0.0"
+VERSION="1.0.1"
+NGINX_V="conf.d"
 
 YUM_CMD=$(which yum)
 APT_CMD=$(which apt)
@@ -31,6 +32,11 @@ if [ "$CONFIG" == "server" ]; then
     if [[ ! -z $YUM_CMD ]]; then yum install -y nginx; elif [[ ! -z $APT_CMD ]]; then apt install -y nginx; else apk add nginx; fi
     sudo mkdir /etc/LoadLinker && sudo cp ../loadlinker/* /etc/LoadLinker/ && sudo rm /etc/loadlinker/loadlinker.service
     sudo cp ./LoadLinker /usr/bin/LoadLinker
+
+    echo "Configuring Nginx..."
+    if [[ -e "/etc/nginx/http.d/default.conf" ]]; then  $NGINX_V="http.d"; fi
+    sudo rm /etc/nginx/$NGINX_V/default.conf
+    sudo mv /etc/loadlinker/default.conf /etc/nginx/$NGINX_V/
 
     echo "Enabling LoadLinker service..."
     sudo mv ./loadlinker/loadlinker.service /etc/systemd/system/
