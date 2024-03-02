75c2b700725b251e36a36c87ddc67bcc8c2e5284
Lucien CARNEIRO <lucien.carneiro@proton.me>
2024-03-01 21:29:56 -0500

    add: new trigger for incomming connections

--- include/server/nginx.hpp
+++ include/server/nginx.hpp
@@ -14,7 +14,7 @@ class nginx {
         int get_server_count() const;
 
     private:
-        const int _check_time = 5;
+        const int _check_time = 1;
         std::string _upstream_path;
         std::vector<std::vector<std::string>> _servers_list;
 };
--- src/server/nginx.cpp
+++ src/server/nginx.cpp
@@ -1,7 +1,5 @@
 #include "server/nginx.hpp"
 
-#include <utility>
-
 nginx::nginx(std::string upstream_path) {
     this->_upstream_path = std::move(upstream_path);
 }
@@ -9,7 +7,6 @@ nginx::nginx(std::string upstream_path) {
 void nginx::register_server(std::string ip_address, int port) {
     std::vector<std::string> new_server;
 
-    std::cout << "Registering server " << ip_address << ':' << port << std::endl;
     for (std::vector<std::string> &server : this->_servers_list) {
         if (server[0] == ip_address && server[1] == std::to_string(port)) {
             server[2] = std::to_string(std::time(nullptr));
@@ -17,11 +14,11 @@ void nginx::register_server(std::string ip_address, int port) {
         }
     }
 
-    std::cout << "Server not found, adding it to the list" << std::endl;
     new_server.push_back(ip_address);
     new_server.push_back(std::to_string(port));
     new_server.push_back(std::to_string(std::time(nullptr)));
     this->_servers_list.push_back(new_server);
+    this->write_configuration_file();
     new_server.clear();
 }
 
@@ -36,12 +33,9 @@ void nginx::check_servers_state() {
     for (int size = 0; size < server_count; size++) {
         if (std::time(nullptr) - std::stoi(this->_servers_list[size][2]) > this->_check_time) {
             this->_servers_list.erase(this->_servers_list.begin() + size);
-            std::cout << "Removing server from the list" << std::endl;
             server_count--, size--;
         }
     }
-    //TODO: Check new server connection
-    //This check verify only if current server are expired
     if (server_count != index)
         this->write_configuration_file();
 }
@@ -50,7 +44,7 @@ void nginx::write_configuration_file() {
     std::ofstream file;
 
     file.open(this->_upstream_path + "/upstream.conf", std::ios::out | std::ios::trunc);
-    for (std::vector<std::string> &server : this->_servers_list)
+    for (std::vector<std::string> const &server : this->_servers_list)
         file << "server " << server[0] << ':' << server[1] << ';' << std::endl;
     file << std::endl;
     file.close();
